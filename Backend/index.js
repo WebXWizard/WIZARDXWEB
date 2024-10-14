ca9544853b9bcf54b91a986ef3c8d654bbbbb912
Shahe Alam <alamshahe9044@gmail.com>
2024-10-14 10:43:10 +0530

    ThunderClient use to add method

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,14 @@
-
-// For Express initial
-
 const express = require('express');
 const app = express();
+const port = 5000;
 
+const UserRouter = require('./routers/user')
 
+// middleware
+app.use(express.json())
 
+app.use('/user', UserRouter)
 
-const port = 5000  // Port Number 
-
-
-//Routing ---
-
-app.get('/', (req,res) => {
-    res.send("Hello World")
+app.listen(port,() => {
+    console.log(`Server is running on port ${port}`)
 })
-
-
-
-// For Running on Server
-
-app.listen(port, () => {
-    console.log(`Server is Running on port ${port}`)
-})
-
